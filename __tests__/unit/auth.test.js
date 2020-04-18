0b615a28434026d742546aa9ae680f86349c33aa
GersonLCSJunior <gersonlcsjunior@gmail.com>
2020-04-18 01:49:19 -0300

    Testes de erro removios. Jest configurado para não parar os testes caso ocorra algum erro

--- __tests__/unit/auth.test.js
+++ __tests__/unit/auth.test.js
@@ -5,16 +5,6 @@ const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
 const { getToken } = require('../../src/authController');
 
 describe('Auth', () => {
-    it('should return an invalid_client error', async () => {
-        const access_token = await getToken({APPLICATION_KEY: "123", REFRESH_TOKEN});
-        expect(access_token.error.error).toBe("invalid_client");
-    })
-
-    it('should return an error', async () => {
-        const access_token = await getToken({APPLICATION_KEY, REFRESH_TOKEN: "454"});
-        expect(access_token.error.error).toBe("invalid_grant");
-    })
-
     it('should return an access token', async () => {
         const access_token = await getToken({APPLICATION_KEY, REFRESH_TOKEN});
         expect(access_token).toBeDefined();
